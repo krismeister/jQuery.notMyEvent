d4505a53c08ab2560e24f7f423a386e68e1745c3
Kris <kris@aliencom.net>
2013-05-28 05:16:46 -0400

    added lint

--- Jakefile.js
+++ Jakefile.js
@@ -1,15 +1,16 @@
 
-var  jake = require("jake");
-var fs = require('fs');
+var jake = require("jake");
+var fs = require('fs-extra');
 var util = require('util');
 var UglifyJS = require('uglify-js');
-// JAKE.task('taskname', [ dependencies ], function(){});
+var lint = require('jake-jshint');
+
 
 /**
  * default task
  */
 desc('Execute all tasks');
-task('default',['minify','copy-assets'], function () {
+task('default',['lint','minify','copy-assets'], function () {
     console.log('* All Tasks complete');
 });
 
@@ -29,19 +30,48 @@ task('minify', function(){
     var out = fs.openSync('dist/jquery.notMyEvent.min.js', 'w+');
     fs.writeSync(out, result.code);
 
-    console.log('minify : complete')
 });
 
+desc("Copy the assets");
 task('copy-assets',function(){
 
     console.log('---------- copy-assets');
+    fs.copy('src/', 'dist/', function(err){
+        if (err) {
+            fail(console.error(err));
+        }
+        else {
+            console.log("success!")
+        }
+    }); //copies file
+
+
+
 
     //read in the file:
-    var index  = fs.readFileSync('src/index.html').toString();
+    var index  = fs.readFileSync('dist/index.html').toString();
     index = index.split('notMyEvent.js').join('notMyEvent.min.js');
     var out = fs.openSync('dist/index.html', 'w+');
     fs.writeSync(out, index);
 
-    console.log('copy-assets : complete');
 
+});
+
+desc("Lint the code");
+task("lint", [], function() {
+
+    console.log('---------- lint the code');
+    var files = new jake.FileList();
+    files.include("src/*.js");
+
+    var options = {
+        node: true
+    };
+
+    var globals = {
+        describe: false
+    };
+
+    var pass = lint.validateFileList(files.toArray(), options, globals);
+    if (!pass) fail("Lint failed");
 });
